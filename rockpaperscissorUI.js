2dd0440fd4eda5c908c27990183758f7beebf9ff
osseko <253503304+osseko@users.noreply.github.com>
2026-02-28 20:59:57 +0800

    have simple UI, and scoring system

--- rockpaperscissorUI.js
+++ rockpaperscissorUI.js
@@ -7,51 +7,56 @@ let btnRock = document.querySelector("#rock");
 let btnPaper = document.querySelector("#paper");
 let btnScissors = document.querySelector("#scissors");
 
+const humanChoose = document.getElementById("player");
+const computerChoose = document.getElementById("computer");
 
+const humanPoints = document.getElementById("playerScore");
+const computerPoints = document.getElementById("computerScore");
 
-let humanScore = 1;
+const roundResult = document.getElementById("declare");
 
-let computerScore = 1;
+let humanScore = 0;
+let computerScore = 0;
+
 
 
 function playRound(comPick, playerPick){
     
-     //const isWin = "rock" > "scissors" && "scissors" > "paper" && "paper" > "rock";
-
+     
      if (comPick === playerPick){
                     return console.log("player", playerPick,"com", comPick, "draw"),
                          "draw" 
 
           }    else if (comPick === "rock" && playerPick === "scissors"){
                     return console.log("com", comPick, 
-                                        "player", playerPick, "computerScore"),
-                         "com point", computerScore++;
+                                        "player", playerPick, "comScore"),
+                         computerScore++;
                                    
           }    else if (comPick === "scissors" && playerPick === "paper"){
                     return console.log("com", comPick, 
-                                        "player", playerPick, "computerScore"), 
-                         "com point", computerScore++;
+                                        "player", playerPick, "comScore"), 
+                         computerScore++;
                                        
           }    else if (comPick === "paper" && playerPick === "rock"){
                     return console.log("com", comPick, 
-                                        "player", playerPick, "computerScore"), 
-                         "com point", computerScore++;
+                                        "player", playerPick, "comScore"), 
+                         computerScore++;
                                         
           }    else if (playerPick === "rock" && comPick === "scissors"){
                     return console.log("player", playerPick,
                                         "com", comPick, "humanScore"),
-                         "player point", humanScore++;
+                         humanScore++;
                                                            
           }    else if (playerPick === "scissors" && comPick === "paper"){
                     return console.log("player", playerPick,
                                         "com", comPick, "humanScore"),
-                         "player point", humanScore++;
+                         humanScore++;
                                         
 
           }    else if (playerPick === "paper" && comPick === "rock"){
                     return console.log("player", playerPick,
                                         "com", comPick, "humanScore"), 
-                         "player point", humanScore++;
+                         humanScore++;
                                   
           }    
      
@@ -63,42 +68,39 @@ function getComputerChoice(computer){
      return computer = choices[Math.floor(Math.random() * choices.length)];
 }
 
-
-
-function playGame(){   
-          
-          let final;
-          
-          if (humanScore === 5){
-               console.log("You are winner");
-          } else if (computerScore === 5){
-               console.log("try again");
-          }
-
-          return final;
-
-     }
-
-
-
-
 const clicker = buttons.forEach((button) => {
 
-     button.addEventListener("click", () => {
+     button.addEventListener("mousedown", () => {
           const clickedOn = button.value;
           
           const comChoice = getComputerChoice();
          
           const gameRound = playRound(comChoice, clickedOn);
 
-          console.log("play", gameRound);
-          const finalResult = playGame();
-          console.log(finalResult);
+          humanChoose.innerText = "player: " + clickedOn;
+          computerChoose.innerText = "computer: " + comChoice;        
+     });
+
+     button.addEventListener("mouseup", () => {
+
+          console.log("score", humanScore, computerScore);
+          if(humanScore === 5){
+               console.log("Player win")
+               roundResult.innerText = "results: " + "You Win!"
+          } else if (computerScore === 5){
+               console.log("try again")
+               roundResult.innerText = "results: " + "com wins"
+          }
+          
+          humanPoints.innerText = "score: " + humanScore;
+          computerPoints.innerText = "score: " + computerScore;
+         
+
 
      });
 });
 
-    
+
 
 
 
